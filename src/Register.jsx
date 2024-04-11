1717e7088cafa2a950e8accb7caf3e9155807aa0
JD10S <dodevp10@gmail.com>
2024-04-11 11:00:35 -0500

    Register conectado al back

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,25 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import Cookies from 'universal-cookie';
+import axios from 'axios';
 
 const Register = () => {
+    const baseUrl ="https://localhost:7169/UserControllers";
+    const navigate = useNavigate();
+    const cookies = new Cookies();
+    const [form, setForm]= useState({
+        userName:'',
+        password:''
+    });
 
-    const navigate = useNavigate(); 
+    const [error, setError] = useState('');
+    const handleChange=e=>{
+        const {name, value} = e.target;
+        setForm({
+            ...form,
+            [name]:value
+        });
+        console.log(form)
+    }
 
     const redirectToLogin = () => {
         navigate('/Loguin');
     };
+  
+
+    const Registrarse=async()=>{
+        try {
+            const response = await axios.post(`${baseUrl}?userName=${form.userName}&password=${form.password}`);
+            const data = response.data;
+            if (response.status === 200) { 
+                alert("Usuario registrado correctamente: " + form.userName);
+                navigate('/Loguin');
+            } else {
+                setError(data.message || 'Error al registrar usuario');
+            }
+        } catch (error) {
+            console.log(error);
+            setError('Error al registrar usuario: ' + error.message);
+        }
+    }
 
     return (
         <div className="r-container">
             <span className='title'>Notes App</span>
             <span className='sub-title'>Register</span>
-            <form >
-                <input type='text' placeholder='Name'></input>
-                <input type='password' placeholder='password'></input>
-                <button>Register</button>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+            <form onSubmit={e => e.preventDefault()}>
+                <input name='userName' type='text' placeholder='Name' value={form.userName} onChange={handleChange}></input>
+                <input name='password' type='password' placeholder='Password' value={form.password} onChange={handleChange}></input>
+                <button onClick={Registrarse}>Register</button>
             </form>
-            <p>Tienes cuenta? <span onClick={redirectToLogin}>Loguin</span></p>
+            <p>¿Ya tienes cuenta? <span onClick={redirectToLogin}>Login</span></p>
         </div>
     );
 }
