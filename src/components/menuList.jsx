c84e05d5797746c767cdb34282e4f2170bdb3de3
JD10S <dodevp10@gmail.com>
2024-04-26 23:33:50 -0500

    funtional edit and delete

--- src/components/menuList.jsx
+++ src/components/menuList.jsx
@@ -1,24 +1,35 @@
 import React, { useState, useEffect } from "react";
-import { Button, Input, message } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
+import { Button, Input, message,Popconfirm } from "antd";
+import { PlusOutlined,EditOutlined,DeleteOutlined } from "@ant-design/icons";
 import axios from "axios";
 import Cookies from 'universal-cookie';
+import { indexedDBLocalPersistence } from "firebase/auth";
 
-const MenuList = ({ setSelectedCategory }) => {
+const MenuList = ({ onsetSelectedCategory }) => {
   const baseUrl = "https://localhost:7169/CategoryControllers";
   const cookies = new Cookies();
   const [categories, setCategories] = useState([]);
   const [newCategoryName, setNewCategoryName] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
-  useEffect(() => {
-    const savedCategories = JSON.parse(localStorage.getItem("categories")) || [];
-    setCategories(savedCategories);
-  }, []);
 
-  useEffect(() => {
-    localStorage.setItem("categories", JSON.stringify(categories));
-  }, [categories]);
+  const loadCategory = async () =>{
+    try {
+      const response = await axios.get(`${baseUrl}/${cookies.get('userId')}`);
+      console.log(response)
+      setCategories(response.data)
+    } catch (error) {
+      
+    }
+  }
+  useEffect(()=>{
+    loadCategory().then()
+  },[])
 
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+  
   const handleChange = (e) => {
     setNewCategoryName(e.target.value);
   };
@@ -27,9 +38,8 @@ const MenuList = ({ setSelectedCategory }) => {
     try {
       if (newCategoryName.trim() !== "") {
         const response = await axios.post(`${baseUrl}?Name=${newCategoryName}&IdUser=${cookies.get('userId')}`);
-        const newCategory = response.data;
-        setCategories([...categories, { Name: newCategoryName }]);
         setNewCategoryName("");
+        loadCategory().then();
         message.success("Categoría añadida correctamente.");
       } else {
         message.error("Por favor ingresa el nombre de la categoría.");
@@ -39,6 +49,33 @@ const MenuList = ({ setSelectedCategory }) => {
       message.error("Error al agregar la categoría. Por favor, intenta de nuevo más tarde.");
     }
   };
+
+  const handleEdit = async (idCategory) =>{
+    try {
+      if (newCategoryName.trim() !== "") {
+        const response = await axios.put(`${baseUrl}?Name=${newCategoryName}&idCategory=${idCategory}&IdUser=${cookies.get('userId')}`);
+        setNewCategoryName("");
+        loadCategory().then();
+        message.success("Categoría editada correctamente.");
+      } else {
+        message.error("Por favor ingresa el nombre de la categoría.");
+      }
+    } catch (error) {
+      console.error("Error al editar la categoría:", error);
+      message.error("Error al editar la categoría. Por favor, intenta de nuevo más tarde.");
+    }
+  };
+  
+
+  const handleDelete = async (idCategory) =>{
+    try {
+      const response = await axios.delete(`${baseUrl}?id=${idCategory}`)
+      loadCategory().then();
+    } catch (error) {
+      
+    }
+    
+  }
 
   return (
     <div className="menu-container">
@@ -58,10 +95,27 @@ const MenuList = ({ setSelectedCategory }) => {
       </Button>
       {categories.length > 0 && (
         <div className="category-list-container">
-          {categories.map((category, index) => (
-            <div key={index}  className="category-item"  style={{ color: "white" }}>
-              {category.Name}
+          {categories.map(category => (
+            <div
+              key={indexedDBLocalPersistence} 
+              className={`category-item ${selectedCategory === category ? 'selected' : ''}`}
+              style={{ color: "white" }}
+              onClick={() => handleCategoryClick(category)}
+            >
+              {category.name}
+              <div className="category-actions">
+                        <EditOutlined onClick={() => handleEdit(category.idCategory)} />
+                        <Popconfirm
+                          title="¿Estás seguro que quieres eliminar esta categoría?"
+                          onConfirm={() => handleDelete(category.idCategory)}
+                          okText="Sí"
+                          cancelText="No"
+                        >
+                          <DeleteOutlined />
+                        </Popconfirm>
+                      </div>
             </div>
+            
           ))}
         </div>
       )}
@@ -70,6 +124,3 @@ const MenuList = ({ setSelectedCategory }) => {
 };
 
 export default MenuList;
-
-
- 
